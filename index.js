031738459e2a99c8f7fee1750cf619bc9a80e608
Macbook <tahreer.90z@gmail.com>
2022-01-06 19:53:43 +0300

    JS Task Done

--- index.js
+++ index.js
@@ -7,5 +7,26 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
+height = height / 100;
+let BMI = (weight / (height * height));
+alert(`Your BMI result is ${BMI}`)
+
+if ( BMI < 18.5 ) {
+  alert("Underweight")
+}
+  else if (BMI > 18.5 && BMI < 24.9){
+  alert("Healthy Weight")
+  }
+  else if (BMI > 25.0 && BMI < 29.9){
+  alert("Overweight")
+}
+else if ( BMI > 30.0) { 
+  alert("Above Obesity")
+}
+
+
   // You are going to write the code here before the curly braces 👇🏻
 }
+
+
+
